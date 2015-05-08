5f34fbfd77151bac2d553d5bf91b6e7fd51108ea
Dmitry Kireev <dmitry@kireev.co>
2015-05-08 14:59:46 -0700

    basic sming init working

--- sming.py
+++ sming.py
@@ -4,6 +4,8 @@ import os
 from jinja2 import Environment, FileSystemLoader
 import argparse
 from git import Repo
+import codecs
+from github import Github
 from pprint import pprint
 PATH = os.path.dirname(os.path.abspath(__file__))
 TEMPLATE_PATH = os.path.join(PATH, os.path.join('templates'))
@@ -21,6 +23,13 @@ subparsers = parser.add_subparsers(
     help='Run sming {command} -h for additional help'
 )
 subparsers.required = True
+
+
+# List
+init_parser = subparsers.add_parser(
+    'list',
+    help='List templates'
+)
 
 
 # Init
@@ -49,10 +58,11 @@ init_parser.add_argument(
 args = parser.parse_args()
 
 
-
 TEMPLATE_ENVIRONMENT = Environment(
     autoescape=False,
-    loader=FileSystemLoader(TEMPLATE_PATH),
+    loader=FileSystemLoader(
+        searchpath="/"
+    ),
     trim_blocks=False)
 # print(os.path.join(PATH, os.path.join('templates', args.filename)))
 
@@ -61,60 +71,52 @@ class Sming:
     def _render_template(self, template_filename, context):
         return TEMPLATE_ENVIRONMENT.get_template(template_filename).render(context)
 
-    def clone_template(self):
-        Repo.clone_from('git@github.com:kireevco/sming-sample_project.git', args.project_name)
+    @staticmethod
+    def list_templates():
+        g = Github()
+
+        for repo in g.get_user().get_repos():
+            if repo.name.startswith("sming-"):
+                print(repo.name)
+        for repo in g.get_user('anakod').get_repos():
+            if repo.name.startswith("sming"):
+                print(repo.name)
+
+    @staticmethod
+    def clone_template():
+        Repo.clone_from('git@github.com:kireevco/sming-'+args.project_name+'.git', args.project_name)
 
     def create_project(self):
-        walk_dir = os.path.join(TEMPLATE_PATH,args.project_name)
-        #
-        # print('walk_dir = ' + walk_dir)
-        #
-        # # If your current working directory may change during script execution, it's recommended to
-        # # immediately convert program arguments to an absolute path. Then the variable root below will
-        # # be an absolute path as well. Example:
-        # # walk_dir = os.path.abspath(walk_dir)
-        # print('walk_dir (absolute) = ' + os.path.abspath(walk_dir))
-        #
-        # for root, subdirs, files in os.walk(walk_dir):
-        #     print('--root (relative) = ' + os.path.relpath(root, walk_dir))
-        #     print('--\nroot = ' + root)
-        #     list_file_path = os.path.join(root, 'my-directory-list.txt')
-        #     print('list_file_path = ' + list_file_path)
-        #
-        #     # os.mkdir(os.path.join(os.getcwd(),os.path.relpath(root, walk_dir)))
-        #     # print('creating '+os.path.join(os.getcwd(),os.path.relpath(root, walk_dir)))
-        #
-        #     with open(list_file_path, 'wb') as list_file:
-        #         for subdir in subdirs:
-        #             print('\t- subdirectory ' + subdir)
-        #
-        #         for filename in files:
-        #             file_path = os.path.join(root, filename)
-        #
-        #             print('\t- file %s (full path: %s)' % (filename, file_path))
-        #
-        #             with open(file_path, 'rb') as f:
-        #                 context = {'project_name':'test'}
-        #                 f_content = f.read()
-        #                 list_file.write(('The file %s contains:\n' % filename).encode('utf-8'))
-        #                 list_file.write(f_content)
-        #                 list_file.write(b'\n')
-        #                 #os.path.join(os.getcwd(),filename)
-        #                 html = self._render_template(os.path.join(root,filename), context)
-        #                 print(html)
-        #                 f.write(html)
-        #
+        self.clone_template()
 
+        walk_dir = os.path.join(os.getcwd(), args.project_name)
 
+        # If your current working directory may change during script execution, it's recommended to
+        # immediately convert program arguments to an absolute path. Then the variable root below will
+        # be an absolute path as well. Example:
+        # walk_dir = os.path.abspath(walk_dir)
+        print('walk_dir (absolute) = ' + os.path.abspath(walk_dir))
 
-
-
+        for root, subdirs, files in os.walk(walk_dir):
+            for file in files:
+                file_path = os.path.join(root, file)
+                print(file_path)
+                if not 'git' in root:
+                    context = {'project_name': args.project_name}
+                    result = self._render_template(file_path, context)
+                    with open(file_path, 'w') as template:
+                        print(file_path)
+                        print(result)
+                        template.write(result)
+                    template.close()
 
 
 def main():
     sming = Sming()
-    sming.clone_template()
-    # sming.create_project()
+    if args.operation == 'init':
+        sming.create_project()
+    elif args.operation == 'list':
+        sming.list_templates()
 
 ########################################
 
